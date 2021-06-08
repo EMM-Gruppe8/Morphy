b2e806dd26f0a45e5d94f8d0e12f5c305b8db018
Bennett <hello@vantezzen.io>
2021-06-08 10:17:15 +0200

    Develop Level Controller

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -12,14 +12,28 @@ public class LevelController : MonoBehaviour
     // This will be persisted on the hard drive
     private static int highestCompletedLevel = 0;
 
+    // Save if we already loaded the data from disk
+    private static bool hasLoaded = false;
+
+    private static readonly int MAX_LEVEL = 3;
+
     // Get the highest level the user completed
     public static int getHighestCompletedLevel() {
+        // Make sure we are setup first
+        setup();
         return highestCompletedLevel;
     }
 
     public static void setHighestCompletedLevel(int val) {
+        // Make sure we are setup first
+        setup();
+
         if (val <= highestCompletedLevel) {
             Debug.Log("Player is already at a higher level");
+            return;
+        }
+        if (val > MAX_LEVEL) {
+            Debug.Log("Trying to set level higher than levels exist");
             return;
         }
         highestCompletedLevel = val;
@@ -29,8 +43,11 @@ public class LevelController : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-    // Load the existing persisted data from the disk if it exists
-    public static void loadPersistedData() {
-        highestCompletedLevel = PlayerPrefs.GetInt("highestCompletedLevel", 0);
+    // Load the existing persisted data from the disk if it exists and if we are not already setup
+    private static void setup() {
+        if (!hasLoaded) {
+            highestCompletedLevel = PlayerPrefs.GetInt("highestCompletedLevel", 0);
+            hasLoaded = true;
+        }
     }
 }
--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -10,6 +10,24 @@ public class MainMenuScript : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject levelSelection;
+    public GameObject[] levels;
+
+    // Setup dynamic parts of the menu
+    public void Start() {
+        updateDisabledLevels();
+    }
+
+    private void updateDisabledLevels() {
+        // Enable all levels that the user has unlocked
+        int enabledLevels = LevelController.getHighestCompletedLevel();
+        Debug.Log("Enabeling level selection up to level " + (enabledLevels + 1));
+        for(int i = 0; i <= enabledLevels; i++) {
+            if (levels.Length > i) {
+                levels[i].transform.Find("Disabled").gameObject.SetActive(false);
+                Debug.Log("Enabling Level" + (i + 1));
+            }
+        }
+    }
 
     /**
      * Open the Game Scene to start the game
@@ -24,6 +42,14 @@ public class MainMenuScript : MonoBehaviour
      */
     public void OpenLevel(int level) {
         Debug.Log("Start Game at level " + level);
+        if (LevelController.getHighestCompletedLevel() + 1 < level) {
+            Debug.Log("Level is not unlocked yet");
+            return;
+        }
+
+        LevelController.setHighestCompletedLevel(level);
+        updateDisabledLevels();
+
         // TODO: Implement when Game Engine is ready
     }
 
