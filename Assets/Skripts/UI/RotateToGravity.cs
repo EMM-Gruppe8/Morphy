6c387c3958f56f95c7c83352f08a665e376f03c6
zink-l <leon.enzenberger@protonmail.com>
2021-06-14 11:21:08 +0200

    Add clarifing comments

--- Assets/Skripts/UI/RotateToGravity.cs
+++ Assets/Skripts/UI/RotateToGravity.cs
@@ -9,9 +9,13 @@ public class RotateToGravity : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //
+        // Measure Angle Between x and y axis and convert to deg.
         float rotation = Mathf.Atan2(-Input.acceleration.x, -Input.acceleration.y)*Mathf.Rad2Deg;
+
+        // Smooth the movement out to not make the controlls jitter
         smoothedRotation = Mathf.LerpAngle(smoothedRotation, rotation, 25.0f * Time.deltaTime);
+
+        //set the angle of the controlls negativ to move against the rotation of the device
         transform.eulerAngles = new Vector3(0,0, -smoothedRotation);
     }
 }
